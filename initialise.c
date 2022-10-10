c4ad844142193c150625c4f00520b04f8b77ba38
Ayienda Brian <ayiendabrian@gmail.com>
2022-10-10 21:18:55 +0200

    free

--- initialise.c
+++ initialise.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 /**
 * execute - executes the opcode
-* @stack: head ll - stack
+* @stack: head linked list - stack
 * @counter: line_counter
 * @file: poiner to monty file
 * @content: line content
-* Return: nothing
+* Return: no return
 */
 int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
